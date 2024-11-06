0c8788a0a0bf9a502ad10cac6796ca9a7a4fd710
Carlos barbosa <carloslk18@gmail.com>
2024-11-06 03:40:47 -0300

    Inicio de projeto TargetS

---
+++ Q05.cs
@@ -0,0 +1,27 @@
+//Questao 05
+using System;
+
+class Q05{
+    static void Main(){
+        Console.WriteLine("Informe a string: ");
+        string input = Console.ReadLine();
+
+        char[] caract = input.ToCharArray();
+
+        int inicio = 0;
+        int fim = caract.Length -1;
+
+        while (inicio < fim){
+            char temp = caract[inicio];
+            caract[inicio] = caract[fim];
+            caract[fim] = temp;
+
+            inicio++;
+            fim--;
+        }
+
+        string res = new string(caract);
+
+        Console.WriteLine("String invertida: {0}", res);
+    }
+}
